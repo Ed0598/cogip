a862c0e61fe7c52cd5e31c5b6de6353643ad2b64
Edouard de Romrée <deromree.edouard@gmail.com>
2023-02-01 16:50:28 +0100

    Request sql

--- assets/php/request.php
+++ assets/php/request.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 function selectHome($table){
 try
 {
@@ -14,19 +11,30 @@ catch(Exception $e)
     // En cas d'erreur, on affiche un message et on arrête tout
         die('Erreur : '.$e->getMessage());
 }
-    $last= 'SELECT * from '.$table.'order by created_at DESC limit 5 ';
+    $last= 'SELECT * from '.$table.' order by created_at DESC limit 5 ';
     $ps= $bdd->prepare($last);
     $ps->execute();
-    foreach($ps as $elem){
-        echo "<tr>
-        <td>".$elem['name']."</td>
-        <td>".$elem['phone']."</td>
-        <td>".$elem['email']."</td>
-        <td>".$elem['company_id']."</td>
-        <td>".$elem['created_at']."</td>
-        </tr>";
-    }
     return $ps->fetchAll();
 };
+
+function selectTables($table)
+{
+    try
+    {
+        // On se connecte à MySQL
+        $bdd = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+    }
+    $tables= 'SELECT * from '.$table.' order by created_at DESC';
+    $ps=$bdd->prepare($tables);
+    $ps->execute();
+};
+
+
+
 
 ?>
