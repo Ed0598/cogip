fb9941841e3a1382279da00a8244f739dac450e7
Edouard de Romree de Vichenet <deromree.edouard@gmail.com>
2023-02-02 13:54:54 +0100

    Ajout contacts

--- work/index.php
+++ work/index.php
@@ -20,6 +20,19 @@ $router->get('/contacts/(\d+)', function ($id) {
     echo json_encode(selectTables('contacts',$id));
 });
 
+$router->mount('/contacts',function() use($router){
+    $defaultRequest='SELECT contacts.name, contacts.email, contacts.phone contacts.created_at companies.name AS name FROM contacts JOIN companies ON companies.id = contacts.company_id';
+    $router->get('/five',function() use ($defaultRequest){
+        echo json_encode(createRequest($defaultRequest.' order by created_at DESC limit 5 '));
+    });
+    $router->get('/all', function () use ($defaultRequest) {
+        echo json_encode(createRequest($defaultRequest.' order by created_at DESC'));
+    });
+    $router->get('/(\d+)', function ($id) use ($defaultRequest) {
+        echo json_encode(createRequest($defaultRequest.' WHERE id='.$id));
+    });
+});
+
 $router->mount('/factures', function () use ($router) {
     $defaultRequest = 'SELECT invoices.ref, invoices.created_at, companies.name AS name FROM invoices JOIN companies ON companies.id = invoices.id_company';
     $router->get('/five', function () use ($defaultRequest) {
@@ -36,13 +49,13 @@ $router->mount('/factures', function () use ($router) {
 $router->mount('/compagnies', function () use ($router) {
     $defaultRequest = 'SELECT companies.name, companies.TVA, companies.country, companies.created_at, types.name AS type FROM companies JOIN types ON companies.type_id = types.id';
     $router->get('/five', function () use ($defaultRequest) {
-        echo json_encode(createRequest($defaultRequest, 1, '', ''));
+        echo json_encode(createRequest($defaultRequest.' order by created_at DESC limit 5 '));
     });
     $router->get('/all', function () use ($defaultRequest) {
-        echo json_encode(createRequest($defaultRequest,2,'created_at',''));
+        echo json_encode(createRequest($defaultRequest.' order by created_at DESC'));
     });
     $router->get('/(\d+)', function ($id) use ($defaultRequest) {
-        echo json_encode(createRequest($defaultRequest,3,'',$id));
+        echo json_encode(createRequest($defaultRequest.' WHERE id='.$id));
     });
 });
 
