f1599a23518046755860742622b2ad0600cea412
Edouard de Romree de Vichenet <deromree.edouard@gmail.com>
2023-02-02 10:41:40 +0100

    fin requêtes

--- assets/php/request.php
+++ assets/php/request.php
@@ -34,6 +34,23 @@ function selectTables($table,$row)
     $ps->execute();
 };
 
+function selectId($table,$id)
+{
+    try
+    {
+        // On se connecte à MySQL
+        $bdd = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+    }
+
+    $selectid= 'SELECT * FROM'.$table.'WHERE id='.$id;
+    $ps= $bdd->prepare($selectid);
+    $ps->execute();
+};
 
 
 
