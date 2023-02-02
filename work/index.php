a7fbecb005072591a520f3c3ba7ba37880269ace
Hugo Goorickx <hugo@goorickx.be>
2023-02-02 10:00:28 +0100

    test

--- work/index.php
+++ work/index.php
@@ -9,10 +9,31 @@ header("Content-Type: application/json");
 $router = new \Bramus\Router\Router();
 
 $router->get('/users', function () {
-    echo json_encode([
-        ['id' => 1, 'name' => 'John Doe'],
-        ['id' => 2, 'name' => 'Jane Doe'],
-    ]);
+    try                 { $pdo = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', ''); }
+    catch(Exception $e) { die('Erreur : '.$e->getMessage()); }
+    // $pdo = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', '');
+    $resultat=$pdo->prepare("SELECT * from contacts");
+    $resultat->execute();
+    $donnees=$resultat->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($donnees);
 });
 
+
+// $router->get('/users', function () {
+//     // echo json_encode([["test" => "tst"], ["test" => "test"]]);
+//     try                 { $bdd = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', ''); }
+//     catch(Exception $e) { die('Erreur : '.$e->getMessage()); }
+//     $array = array();
+//     $last = 'SELECT * from contacts';
+//     $ps = $bdd->prepare($last);
+//     $ps->execute();
+//     $resultat = $ps->fetchAll(PDO::FETCH_ASSOC);
+//     while ($row = $ps->fetchAll(PDO::FETCH_ASSOC))
+//         $array[] = $row;
+//     echo json_encode($array);
+// });
+
 $router->run();
+
+
+
