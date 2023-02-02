7522e50097b510c435eb478a845aeca286743b35
LysieSoyez <Lysie.a.soyez@gmail.com>
2023-02-02 15:49:08 +0100

    html structure

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+                <p>
+                    COGIP
+                </p>
+                <div className="link">
+                    <a href="#">
+                        Home
+                    </a>
+                    <a href="#">
+                        Invoices
+                    </a>
+                    <a href="#">
+                        Companies
+                    </a>
+                    <a href="#">
+                        Contacts
+                    </a>
+                </div>
+                <div className="log">
+                    <a href="#">
+                        Sign up
+                    </a>
+                    <a href="#">
+                        Login
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
+import Header from './Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Header />
 );
-
-
