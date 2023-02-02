64b2a65295fcf181950956c63a08824e38c115eb
Hugo Goorickx <hugo@goorickx.be>
2023-02-02 15:38:57 +0100

    Ajout du code jojo

--- work/index.php
+++ work/index.php
@@ -3,10 +3,21 @@
 require __DIR__ . '/vendor/autoload.php';
 require "../assets/php/request.php";
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header("Content-Type: application/json");
+if (isset($_SERVER['HTTP_ORIGIN']))
+{
+    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+    // you want to allow, and if so:
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 1000');
+}
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) 
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+    exit(0);
+}
 
 $router = new \Bramus\Router\Router();
 
