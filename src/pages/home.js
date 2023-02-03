716a1394ec29035df04040523ba175b350d73334
Nathalie Goffette <nathaliegoffette@gmail.com>
2023-02-03 11:48:33 +0100

    config home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
 import Header from '../Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <ChakraProvider>
+
     <Header />
+
+    </ChakraProvider>
+
 );
+
+
+
