d2bdb83d7250a3e3c22d6abfb205e9e617cd9a67
LysieSoyez <Lysie.a.soyez@gmail.com>
2023-02-06 11:57:00 +0100

    link css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import Menuburg from './Menu';
+import Header from './Header';
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Link } from '@chakra-ui/react'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ChakraProvider>
+        <Header />
         <Menuburg />
     </ChakraProvider>
 );
