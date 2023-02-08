56f64d1182fe97fb662f2bf490790fd65131184f
LysieSoyez <Lysie.a.soyez@gmail.com>
2023-02-08 14:32:24 +0100

    component table

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import Footer from './Footer';
 import Navigation from './Navigation';
 import Manage from './Manage';
 import Work from './Work';
-import SmallTable from './SmallTable';
+import Table from './Table';
 
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider, Link } from '@chakra-ui/react'
+import { ChakraProvider} from '@chakra-ui/react'
 
 
 
@@ -19,10 +19,9 @@ root.render(
             <Navigation />
             <Manage />
         </header>
-        <table>
-        <SmallTable table='contacts' display="five" />
-        </table>
-        <SmallTable />
+        <div className='over'>
+        <Table table='factures' display="five" id="id" td1="ref" td2="update_at" td3="name" td4="created_at" th1="Invoice number" th2="Dates due" th3="Company" th4="Created at" />
+        </div>
         <Work />
         <Footer />
     </ChakraProvider>
