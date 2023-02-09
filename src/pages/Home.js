c2ed9691680c1b383dc8557998471c904fe9be98
Nathalie Goffette <nathaliegoffette@gmail.com>
2023-02-09 12:04:41 +0100

    fix css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,7 +25,9 @@ function Home() {
                     id="id" td1="ref" td2="update_at" td3="name" td4="created_at" 
                     th1="Invoice number" th2="Dates due" th3="Company" th4="Created at" />
                 </div>
-                <img src={Main} id='main' />
+                <div className='img_main'>
+                    <img src={Main} id='main' />
+                </div>
                 <h2>
                     Last contacts
                 </h2>
