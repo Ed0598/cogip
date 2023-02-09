d02d2ff31806b25a404a756cf4badfca0ce1830a
Nathalie Goffette <nathaliegoffette@gmail.com>
2023-02-09 16:54:59 +0100

    dashboard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Invoices from "./pages/Invoices";
 import Contacts from "./pages/Contacts";
 import Companies from "./pages/Companies";
+import NewInvoice from "./pages/Newinvoice";
 import PrivacyPolicy from "./pages/Privacypolicy";
 
 
@@ -14,16 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/NewInvoice" element={<NewInvoice />} />
         <Route path="/Invoices" element={<Invoices />} />
         <Route path="/Contacts" element={<Contacts />} />
         <Route path="/Companies" element={<Companies />} />
         <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-
-
-
-
-
-
 
       </Routes>
     </div> 
