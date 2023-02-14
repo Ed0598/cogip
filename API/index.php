7ea80b1407357dc774ed681dc08b0e637346f59b
Hugo Goorickx <hugo@goorickx.be>
2023-02-14 14:49:24 +0100

    Correction API

--- API/index.php
+++ API/index.php
@@ -8,6 +8,6 @@ require('./Controllers/Login.class.php');
 require('./Controllers/Contact.class.php');
 require('./Controllers/Companies.class.php');
 require('./Controllers/Invoices.class.php');
-require('./Controllers/Createlogin.class.php');
+
 require('./Routes/config.php');
 ?>
