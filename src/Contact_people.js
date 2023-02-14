e836ee0e8dee6be1b9a02aec3c721317a3c6a4a3
LysieSoyez <Lysie.a.soyez@gmail.com>
2023-02-14 14:56:42 +0100

    display contacts

--- src/Contact_people.js
+++ src/Contact_people.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import Boy from '../src/assets/images/Boy.svg';
 
 function ContactPeople(props){
     const [data, setData] = useState([]);
+    let { id } = useParams();
 
     useEffect(() => {
-        let url = 'https://api.hugoorickx.tech/' + props.table + '/' + props.display + "1";
+        let url = 'https://api.hugoorickx.tech/' + props.table + '/' + props.display + '/' + id;
         fetch(url, { method: 'GET' })
             .then((response) => response.json())
             .then((responseData) => setData(responseData.message || []));
@@ -20,9 +22,9 @@ function ContactPeople(props){
                 {Array.isArray(data) ? data.map((elem) => (
                     <div className='contact'>
                         <h3>
-                            Test
                             {elem.name}
                         </h3>
+                        <img src={Boy} alt="#" />
                      </div>
                  )) : null}
             </div>
