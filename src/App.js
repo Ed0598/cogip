f47c004bccf2adbbcf4cda7c42ae35c75a257103
LysieSoyez <Lysie.a.soyez@gmail.com>
2023-02-15 16:52:51 +0100

    all form

--- src/App.js
+++ src/App.js
@@ -34,6 +34,10 @@ const router = createBrowserRouter([
     element: <NewCompany />,
   },
   {
+    path: "/NewContact",
+    element: <NewContact />
+  },
+  {
     path: "/Invoices",
     element: <Invoices />,
   },
