d3611644bc21a0121631e8f47f988463a66c742b
LysieSoyez <Lysie.a.soyez@gmail.com>
2023-02-16 10:41:15 +0100

    route

--- src/FormCompany.js
+++ src/FormCompany.js
@@ -1,31 +1,16 @@
 import {
-    Button,
     Select,
     Input,
     FormControl,
     FormLabel,
-    FormErrorMessage,
-    FormHelperText,
 } from '@chakra-ui/react'
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 
     
      
-function FormCompany(props) {
-    const [data, setData] = useState([]);
-    let { id } = useParams();
-
-    useEffect(() => {
-        let url = 'https://api.hugoorickx.tech/' + props.compagnies;
-        fetch(url, { method: 'POST' })
-            .then((response) => response.json())
-            .then((responseData) => setData(responseData.message || []));
-    }, []);
-    console.log(data)
+function FormCompany() {
     return (
         
-            <FormControl isRequired>
+            <FormControl isRequired action="" methode="POST">
                 <div className='form_label'>
                     <FormLabel>Name: </FormLabel>
                     <Input placeholder='Company name' />
@@ -47,8 +32,6 @@ function FormCompany(props) {
                 </div>
                 <button type="button">Save</button> 
             </FormControl>
-            
-        
     )
 }
 
--- src/FormContact.js
+++ src/FormContact.js
@@ -1,87 +1,30 @@
 import {
-    Button,
-    Select,
     Input,
     FormControl,
-    FormLabel,
-    FormErrorMessage,
-    FormHelperText,
-} from '@chakra-ui/react'
-import React, { useState, useEffect } from 'react';
+    FormLabel
+} from '@chakra-ui/react';
 
 
-function FormContact(props) {
-
-    const [data, setData] = useState([]);
-    const [name, setName] = useState("");
-    const [phone, setPhone] = useState("");
-    const [email, setEmail] = useState("");
-    const [companyId, setCompanyId] = useState("");
-    const [createdAt, setCreatedAt] = useState("");
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const formData = {
-            name: name,
-            email: email,
-            phone: phone,
-            created_at: createdAt,
-          };
-          fetch("https://api.hugoorickx.tech/contacts", {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-            .then((response) => response.json())
-            .then((responseData) => {
-              setData(responseData.data || []);
-              setName("");
-              setPhone("");
-              setEmail("");
-              setCompanyId("");
-              setCreatedAt("");
-            });
-        };
-      
-
-        useEffect(() => {
-            let url = "https://api.hugoorickx.tech/companies/all";
-            fetch(url, { method: 'GET' })
-              .then((response) => response.json())
-              .then((responseData) => setData(responseData.data || [] ));
-          }, []);
+function FormContact() {
     return (
         
-            <FormControl isRequired>
+            <FormControl isRequired action="" methode="POST">
                 <div className='form_label'>
 
                     <FormLabel>Name</FormLabel>
-                    <Input placeholder='Name' 
-                    onChange={(event) => setName(event.target.value)}/>
+                    <Input placeholder='Name' name='name' />
 
                     <FormLabel>Phone</FormLabel>
-                    <Input placeholder='Phone' 
-                    onChange={(event) => setPhone(event.target.value)}/>
+                    <Input placeholder='Phone' name='phone'/>
 
                     <FormLabel>Mail</FormLabel>
-                    <Input placeholder="Mail" size="md" type="email" 
-                    onChange={(event) => setEmail(event.target.value)}/>
+                    <Input placeholder="Mail" size="md" type="email" name='mail'/>
 
                     <FormLabel>Company</FormLabel>
-                    <Select placeholder='Select company'
-                    onChange={(event) => setCompanyId(event.target.value)}>
-
-                        {Array.isArray(data) && data.map((elem) => (
-                            <option key={elem.id} value={elem.compagnies_id}> {elem.name_compagnie} </option>
-                        ))}
-                    </Select>
+                    <Input placeholder='Name of the company' name='company'/>
 
                     <FormLabel>Created at</FormLabel>
-                    <Input placeholder="Select Date and Time" size="md" type="date" 
-                    onChange={(event) => setCreatedAt(event.target.value)}/>
+                    <Input placeholder="Select Date and Time" size="md" type="date" name='createAt' />
 
                 </div>
                 <button type="button">Save</button> 
