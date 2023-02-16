ad85c2a81c9ead9747f797c0bdc13eb8935c5ed5
LysieSoyez <Lysie.a.soyez@gmail.com>
2023-02-16 11:54:11 +0100

    pull

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import * as React from 'react';
-import {createBrowserRouter, RouterProvider, Route, createRoutesFromElements} from "react-router-dom";
-import * as ReactDOM from "react-dom/client";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Invoices from "./pages/Invoices";
@@ -14,71 +14,30 @@ import ShowContacts from "./pages/showcontact";
 import ShowInvoices from "./pages/ShowInvoices";
 import NewContact from "./pages/NewContact";
 import NewCompany from './pages/Newcompany';
+import FormContact from './FormContact';
 
-
-const router = createBrowserRouter([
-  createRoutesFromElements(
-    <>
-
-    <Route path="/" element={<Dashboard />}>
-      <Route path="Company" element={<NewCompany />} />
-      <Route path="Contact" element={<NewContact />} />
-    </Route>
-    </>
-  )
-  // {
-  //   path: "/",
-  //   element: <Home />,
-  // },
-  // {
-  //   path: "/Dashboard",
-  //   element: <Dashboard />,
-  // },
-  // {
-  //   path: "/NewInvoice",
-  //   element: <NewInvoice />,
-  // },
-  // {
-  //   path: "/Newcompany",
-  //   element: <NewCompany />,
-  // },
-  // {
-  //   path: "/NewContact",
-  //   element: <NewContact />
-  // },
-  // {
-  //   path: "/Invoices",
-  //   element: <Invoices />,
-  // },
-  // {
-  //   path: "/Contacts",
-  //   element: <Contacts />,
-  // },
-  // {
-  //   path: "/Companies",
-  //   element: <Companies />,
-    
-  // },
-  // {
-  //   path: "/Companies/:id",
-  //   element: <ShowCompanies />,
-  // },
-  // {
-  //   path: "/PrivacyPolicy",
-  //   element: <PrivacyPolicy />,
-  // },
-  // {
-  //   path: "/Contacts/:id",
-  //   element: <ShowContacts />,
-  // },
-  // {
-  //   path: "/Invoices/:id",
-  //   element: <ShowInvoices />,
-  // },
-]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/NewInvoice" element={<NewInvoice />} />
+        <Route path="/Invoices" element={<Invoices />} />
+        <Route path="/Contacts" element={<Contacts />} />
+        <Route path="/Companies" element={<Companies />} />
+        <Route path="/Companies/:id" element={<ShowCompanies />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+        <Route path="/Contacts/:id" element={<ShowContacts />} />
+        <Route path="/Invoices/:id" element={<ShowInvoices />} />
+
+        <Route path="Dashboard" element={<Dashboard />}>
+          <Route path="NewCompany" element={<NewCompany />} />
+          <Route path="FormContact" element={<FormContact />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
+
 
