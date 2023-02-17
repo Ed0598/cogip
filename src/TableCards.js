bab15fdb572bfd93cd73a37c598f419f83601114
Nathalie Goffette <nathaliegoffette@gmail.com>
2023-02-17 09:03:31 +0100

    modifs

--- src/TableCards.js
+++ src/TableCards.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Table from 'git./Table';
+import Table from './Table';
 
 
 function TableCards(props) {
